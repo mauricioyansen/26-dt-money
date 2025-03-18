52321422accae99f50e9e2faa4a78d56e1796937
Mauricio <mauricio_cy@hotmail.com>
2025-03-18 11:39:19 -0300

    search form

---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background: ${(p) => p.theme["gray-900"]};
+    color: ${(p) => p.theme["gray-300"]};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${(p) => p.theme["gray-500"]};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+    border: 0;
+    padding: 1rem;
+    background: transparent;
+    border: 1px solid ${(p) => p.theme["green-300"]};
+    color: ${(p) => p.theme["green-300"]};
+    font-weight: bold;
+    border-radius: 6px;
+    transition: background-color 0.2s, color 0.2s, border-color 0.2s;
+    cursor: pointer;
+
+    &:hover {
+      background: ${(p) => p.theme["green-500"]};
+      border-color: ${(p) => p.theme["green-500"]};
+      color: ${(p) => p.theme.white};
+    }
+  }
+`;
